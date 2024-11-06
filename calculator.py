7e210a1c09becf961fb9e2f3e84e50a1fc255f69
Viktor <136692827+Targa93@users.noreply.github.com>
2024-11-06 12:14:31 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,3 @@
 # calculator.py
-
 def add(a, b):
     return a + b
-
-if __name__ == "__main__":
-    a = float(input("Enter the first number: "))
-    b = float(input("Enter the second number: "))
-    print("The sum is:", add(a, b))
